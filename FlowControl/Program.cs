f282b6544e3b8047bdef31ec91e72aeeb7565a32
PH_ JAYSON <enverzojayson13@gmail.com>
2025-09-11 00:02:46 +0800

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace FlowControl
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int MAX_MINOR_AGE = 17;
+            const int MIN_SENIOR_AGE = 60;
+
+            var isPWD = true;
+           // var age = 25;
+            Console.WriteLine("Add your age: ");
+            var ageString = Console.ReadLine();
+            var age = int.Parse(ageString);
+
+            if (age <= 17 || age >= 60)
+            {
+                Console.WriteLine("Add miinor and senior discount");
+            }
+            else if (isPWD)
+            {
+                Console.WriteLine("Add PWD discount");
+            }
+            else
+            {
+                Console.WriteLine("No Discount");
+            }
+
+            Console.WriteLine("Are you a pwd?:");
+            var pwdString = Console.ReadLine();
+            var pwd = bool.Parse(pwdString);
+
+            if (pwd == true) {
+                Console.WriteLine("Add discount pwd eh");
+            }
+            else
+            {
+                Console.WriteLine("No dc");
+            }
+    
+   
+       
+        } 
+    }
+}
